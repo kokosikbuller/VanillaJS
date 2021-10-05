fe5e41726815ca164c5ce1a8616365f15bb2af1e
Jevgenij <jevgenij.bogatyrenko@gmail.com>
2021-10-05 15:55:52 +0300

    mainpage basic

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,13 +1,1 @@
-import './styles.css';
-import temp from './templates/test.hbs'
-
-const root = document.getElementById('root')
-
-const markup = temp({
-  firstname: "Yehuda",
-  lastname: "Katz",
-});
-
-root.innerHTML = markup;
-
-
+import './style.css';
