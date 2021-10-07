5b206c75877d6135fdee36f84b57abf646f7b483
Kostya Rizun <k.rizun@splinestudio.ua>
2021-10-07 16:01:09 +0300

    add page oneMovie

---
+++ webpack/src/testAPI.js
@@ -0,0 +1,84 @@
+export const fakeAPI = {
+  id: 1,
+  movieName: 'Веном 2',
+  movieSub: 'Venom: Let There Be Carnage',
+  movieImg: 'https://ex-fs.net/uploads/posts/2021-10/1633118651_venom-2.jpg',
+  like: 100,
+  dislike: 30,
+  rating: 7.7,
+  year: 2021,
+  country: 'США',
+  genre: ['фантастика','боевик','триллер'],
+  translation: 'Дублированный',
+  time: '90 мин. / 01:30',
+  guality: 'SuperTS',
+  age: 16,
+  plot: 'Более чем через год после тех событий журналист Эдди Брок пытается приспособиться к жизни в качестве хозяина инопланетного симбиота Венома, который наделяет его сверхчеловеческими способностями. Брок пытается возродить свою карьеру и берет.',
+  personnelMovie: [
+    {
+      imgUrl: 'https://ex-fs.net/uploads/posts/2021-10/thumbs/1633118212-823186006-ex-fs.net-1.jpg',
+      href: '#',
+    },
+    {
+      imgUrl: 'https://ex-fs.net/uploads/posts/2021-10/thumbs/1633118151-993108105-ex-fs.net-2.jpg',
+      href: '#',
+    },
+    {
+      imgUrl: 'https://ex-fs.net/uploads/posts/2021-10/thumbs/1633118201-1310252048-ex-fs.net-3.jpg',
+      href: '#',
+    },
+    {
+      imgUrl: 'https://ex-fs.net/uploads/posts/2021-10/thumbs/1633118355_32456-2.jpg',
+      href: '#',
+    },
+    {
+      imgUrl: 'https://ex-fs.net/uploads/posts/2021-10/thumbs/1633118388_32456-1.jpg',
+      href: '#',
+    }
+  ],
+  mainRolls: [
+    {
+      imgUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/933167f5-f078-44b6-81f1-157c9c685ff8/280x420',
+      name: 'Том Харди'
+    },
+    {
+      imgUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/933167f5-f078-44b6-81f1-157c9c685ff8/280x420',
+      name: 'Том Харди'
+    },
+    {
+      imgUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/933167f5-f078-44b6-81f1-157c9c685ff8/280x420',
+      name: 'Том Харди'
+    },
+    {
+      imgUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/933167f5-f078-44b6-81f1-157c9c685ff8/280x420',
+      name: 'Том Харди'
+    },
+    {
+      imgUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/933167f5-f078-44b6-81f1-157c9c685ff8/280x420',
+      name: 'Том Харди'
+    }
+  ],
+  produsers: ['Ави Арад', 'Том Харди', 'Келли Марсел'],
+  director: 'Энди Серкис',
+  screenwriter: ['Келли Марсел', 'Том Харди'],
+  operator: 'Роберт Ричардсон',
+  compositor: 'Марко Белтрами',
+  similar: [
+    {
+      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmF8psXqJJps20u4TXX-lWf_YnaBiURGz98g&usqp=CAU',
+      name: 'Веном'
+    },
+    {
+      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmF8psXqJJps20u4TXX-lWf_YnaBiURGz98g&usqp=CAU',
+      name: 'Веном'
+    },
+    {
+      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmF8psXqJJps20u4TXX-lWf_YnaBiURGz98g&usqp=CAU',
+      name: 'Веном'
+    },
+    {
+      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmF8psXqJJps20u4TXX-lWf_YnaBiURGz98g&usqp=CAU',
+      name: 'Веном'
+    },
+  ]
+}
