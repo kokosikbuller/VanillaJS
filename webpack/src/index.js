d48a3b8623884aeec3935fc40ea65eb4ea7c1c3c
Kostya Rizun <k.rizun@splinestudio.ua>
2021-10-09 16:41:12 +0300

    add loader

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,9 +2,13 @@ import './styles.css';
 import temp from './templates/test.hbs'
 import { fakeAPI } from './testAPI';
 import { oneMovie } from './js/oneMovie';
+import { loader } from './js/loader';
 
 const root = document.getElementById('root')
 
 root.innerHTML = oneMovie(fakeAPI);
+
+// root.innerHTML = loader();
+
 
 
